6a177e05bbdd9defbb2e5d04fac123b6640a79be
Max Stern <maxhenrystern@gmail.com>
2017-09-24 19:00:40 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running!")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+
+	else {
+		alert("Sorry, try again");
+	}
+}
+
+	
